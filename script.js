ebb189dd3ee2bbddc9e4e76b6bcd36a1e43dfd33
gegi <35646675+Gary21435@users.noreply.github.com>
2025-03-26 13:10:54 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const gridSize = 800;
+
+const grid = document.querySelector("container");
+
+
+
+for (let i = 0; i < 16; i++) {
+    const square = document.createElement("div");
+    grid.appendChild(square);
+
+    let squareSize = gridSize / 
+}
